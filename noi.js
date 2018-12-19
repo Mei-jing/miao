009255afc444df81a3cb1e3296fef8791df360d3
meijing <meijingmobile@gmail.com>
2018-12-19 20:53:14 +0800

    .

--- noi.js
+++ noi.js
@@ -1,8 +1,16 @@
 var x = +prompt("请输入重量")
 var a = prompt("是否加急 y or n")
 
-if (x<1000 && a==n){
+var b = Math.ceil((x-1000)/500)*4
+var c = b +8
+var d = b +5 +8
+
+if (x<1000 && a==="n"){
     console.log("8")
+}else if (x>1000 && a==="n"){
+    console.log(c)
+} else if (x>1000 && a==="y"){
+    console .log(d)
 }
 
 
