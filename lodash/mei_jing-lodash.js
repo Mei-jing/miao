15e3d03a6b0f7fa80e5004d12cf6a1cd971a14b2
meijing <meijingmobile@gmail.com>
2019-01-10 23:26:18 +0800

    concat

--- lodash/mei_jing-lodash.js
+++ lodash/mei_jing-lodash.js
@@ -2,9 +2,13 @@ var mei_jing = function () {
 
   function chunk(ary, size = 1) {
     let result = []
-    for(let i =0;i<ary.length;i++){
-
+    for (let i = 0; i < ary.length; i++) {
+      result.push(ary.splice(i, i + size))
+      if (ary.length < size) {
+        result.push(ary)
+      }
     }
+    return result
   }
 
   function compact(ary) {
@@ -17,7 +21,32 @@ var mei_jing = function () {
     return result
   }
 
+  function concat(ary, ...value) {
+    let result = []
+    for (let i = 0; i < ary.length; i++) {
+      result.push(ary[i])
+    }
+    let mid = result
+    for (let i = 0; i < value.length; i++) {
+      if (typeof value[i] == "object") {
+        for (let j = 0; j < value[i].length; j++) {
+          mid.push(value[i][j])
+        }
+      } else {
+        mid.push(value[i])
+      }
+    }
+    return mid
+  }
+
+
+
+
+
+
+
   return {
+    concat: concat,
     chunk: chunk,
     compact: compact,
   }
