922a6bcf2eb18e00d5d9f36333d1f62ababdbef0
meijing <meijingmobile@gmail.com>
2019-01-10 12:01:39 +0800

    test4

--- lodash/mei_jing-lodash.js
+++ lodash/mei_jing-lodash.js
@@ -4,13 +4,12 @@ var mei_jing = function(){
 
   }
   function compact(ary) {
-    let result = []
     for(let i =0;i<ary.length;i++){
       if(ary[i] == 0 || ary[i] == null || ary[i] == "" || ary[i] == undefined ){
-         result = ary.splice(i,1)
+         ary.splice(i,1)
       }
     }
-    return result
+    return ary
   }
 
   return {
